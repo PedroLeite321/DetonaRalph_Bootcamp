5ff4683c42b4d41ce6966af2591436d0a4d6e592
pedro <pk.bleite@gmail.com>
2023-12-26 23:45:24 -0300

    game grid added / tabela utilizada no jogo adicionada

--- scripts/loadgame.js
+++ scripts/loadgame.js
@@ -1,10 +1,14 @@
 function loadGameBtn()  {
     return function wasClicked()    {
         const loading = document.getElementById("loadScreen");
+        const gamerow = document.getElementById("game-row");
+        startBtnId = document.getElementById("start-gameBtn");
         const startButton = document.querySelector("#start-gameBtn").addEventListener('click', (e) =>{
             loading.style.display = "flex";
             setTimeout(() => {
                 loading.style.display = "none";
+                startBtnId.style.display = "none";
+                gamerow.style.display = "grid";
             }, 5300)
         })
     }
