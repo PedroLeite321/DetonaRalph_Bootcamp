830f7d79be6a4197e737c869a5693157b0fafa76
pedro <pk.bleite@gmail.com>
2023-12-27 22:29:03 -0300

    starting game engine / começando engine do jogo

---
+++ scripts/engine.js
@@ -0,0 +1,14 @@
+const state = {
+    view:   {
+
+        square: document.querySelectorAll(".square"),
+        enemy: document.querySelector(".enemy"),
+        timeLeft: document.querySelector(".menu-time"),
+        score: document.querySelector(".menu-score"),
+        
+         
+        
+    },
+    value: {},
+
+}
