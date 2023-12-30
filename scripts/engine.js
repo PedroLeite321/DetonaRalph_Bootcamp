8723568300b9868756049ca63062900e76368630
pedro <pk.bleite@gmail.com>
2023-12-30 01:38:27 -0300

    felix and ralph being set to random positions | felix e ralph em posições diferentes.

--- scripts/engine.js
+++ scripts/engine.js
@@ -1,56 +1,41 @@
 const state = {
-    view:   {
-         square: document.querySelectorAll(".square"),
-         time_left: document.querySelector("#time-left"),
-         score: document.querySelector("#score").innerHTML,
-         lifes: document.querySelector("#lives"),
-        
+    view: {
+        square: document.querySelectorAll(".square"),
+        time_left: document.querySelector("#time-left"),
+        score: document.querySelector("#score").innerHTML,
+        lifes: document.querySelector("#lives"),
     },
     values: {},
+};
 
-}
-const initialize = () =>    {
-    const cleanUpClasses = ()   =>  {
+const randomSquare = () => {
+    state.view.square.forEach((square) => {
+        square.classList.remove("enemy", "hero");
+    });
 
-        state.view.square.forEach((square) => {
-            square.classList.remove("enemy");
-            square.classList.remove("hero");
-        });
-                
+    let randomNumber = Math.floor(Math.random() * 9);
+    let randomNumber2;
 
+    if(randomNumber2 === undefined || randomNumber2 === null || randomNumber2 === randomNumber)  {
+        randomNumber2 = Math.floor(Math.random() * 9);
     }
-    setTimeout(() => {
-        
-        cleanUpClasses();
-        initialize();
-    }, 1000);
+
+    let randomSquareEnemy = state.view.square[randomNumber];
+    let randomSquareHero = state.view.square[randomNumber2];
+
+    randomSquareEnemy.classList.add("enemy");
+    randomSquareHero.classList.add("hero");
+
     
-    const randomSquare = () =>  {
+};
 
-        let randomNumber = Math.floor(Math.random() * 9);
-        let randomNumber2 = Math.floor(Math.random() * 9);
-        let randomSquareEnemy = state.view.square[randomNumber];
-        let randomSquareHero;
-        
-        
-
-
-        if(randomNumber !== randomNumber2)  {
-            randomSquareHero = state.view.square[randomNumber2];
-        }else{
-            initialize();
-        }
-        randomSquareEnemy.classList.add("enemy");
-        randomSquareHero.classList.add("hero");
-    
-        
-    }
-    if(state.view.score == 10 || state.view.score < 0)   {
-        console.log("Bigger than ten");
+const initialize = () => {
+    const runRandomSquareWithTimeout = () => {
         randomSquare();
-    }else   {
-        initialize();
-    }
+        setTimeout(runRandomSquareWithTimeout, 900);
+    };
 
-}
+    runRandomSquareWithTimeout();
+};
+
 initialize();
