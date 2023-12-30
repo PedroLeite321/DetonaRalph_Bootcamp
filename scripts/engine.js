bda1db83eed7a68ed9df6eb89a7436909244490d
pedro <pk.bleite@gmail.com>
2023-12-30 00:14:38 -0300

    recursion failed / recursão falhou

--- scripts/engine.js
+++ scripts/engine.js
@@ -1,14 +1,56 @@
 const state = {
     view:   {
-
-        square: document.querySelectorAll(".square"),
-        enemy: document.querySelector(".enemy"),
-        timeLeft: document.querySelector(".menu-time"),
-        score: document.querySelector(".menu-score"),
-        
-         
+         square: document.querySelectorAll(".square"),
+         time_left: document.querySelector("#time-left"),
+         score: document.querySelector("#score").innerHTML,
+         lifes: document.querySelector("#lives"),
         
     },
-    value: {},
+    values: {},
 
 }
+const initialize = () =>    {
+    const cleanUpClasses = ()   =>  {
+
+        state.view.square.forEach((square) => {
+            square.classList.remove("enemy");
+            square.classList.remove("hero");
+        });
+                
+
+    }
+    setTimeout(() => {
+        
+        cleanUpClasses();
+        initialize();
+    }, 1000);
+    
+    const randomSquare = () =>  {
+
+        let randomNumber = Math.floor(Math.random() * 9);
+        let randomNumber2 = Math.floor(Math.random() * 9);
+        let randomSquareEnemy = state.view.square[randomNumber];
+        let randomSquareHero;
+        
+        
+
+
+        if(randomNumber !== randomNumber2)  {
+            randomSquareHero = state.view.square[randomNumber2];
+        }else{
+            initialize();
+        }
+        randomSquareEnemy.classList.add("enemy");
+        randomSquareHero.classList.add("hero");
+    
+        
+    }
+    if(state.view.score == 10 || state.view.score < 0)   {
+        console.log("Bigger than ten");
+        randomSquare();
+    }else   {
+        initialize();
+    }
+
+}
+initialize();
